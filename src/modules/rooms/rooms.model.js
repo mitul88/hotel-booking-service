b4af35a6df6b5b06d50821750770759bcda1666e
shahed <shahed.rahman88@gmail.com>
2023-10-13 23:42:43 +0600

    rooms model moddified

--- src/modules/rooms/rooms.model.js
+++ src/modules/rooms/rooms.model.js
@@ -1,19 +1,16 @@
 const { Schema, model } = require('mongoose');
 
-const facilities = Schema({
-    breakfast: Boolean,
-    gymnesium: Boolean,
-    parking: Boolean,
-    restaurant: Boolean,
-    swimming_pool: Boolean,
-    wifi: Boolean,
-},{ _id : false })
 
 module.exports.Rooms = model('Rooms', Schema({
     title: String,
     description: String,
-    facilities: facilities,
+    hotelId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Hotel',
+        required: true
+    },
     cost: {
-        type: Number
+        type: Number,
+        required: true
     }
 }, {timestamps: true }) )
